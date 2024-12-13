f2b7345f2ed79e40dc979c5c0aec0818d424c494
Diana Tuz <dianatuz12@gmail.com>
2024-12-13 15:38:18 +0200

    Add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { CartPage, CatalogPage, HomePage, ItemCard } from './pages';
 
 export const App = () => (
-  <div className="App">
-    <h1>Product Catalog</h1>
-  </div>
+  <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/catalog" element={<CatalogPage />} />
+    <Route path="/cart" element={<CartPage />} />
+    <Route path="/phone/:id" element={<ItemCard />} />
+    <Route path="*" element={<h1>404 Page not found</h1>} />
+  </Routes>
 );
